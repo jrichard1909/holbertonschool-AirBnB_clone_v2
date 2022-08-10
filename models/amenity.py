5ee68aad738ac143bb9f9dd66353bd938b27d06a
WardenCode <diegojeanluck@hotmail.com>
2022-08-10 16:07:51 -0500

    Update: adding imports of amenities

--- models/amenity.py
+++ models/amenity.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+import models
 
 
 class Amenity(BaseModel):
