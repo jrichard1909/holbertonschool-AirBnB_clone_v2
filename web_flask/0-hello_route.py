6829997a1661994050794789eaa446cc797b025b
jrichard1909 <jrichard1909@gmail.com>
2022-11-08 18:57:39 -0500

    task 0. Hello Flask!

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from flask import Flask, request
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello():
+    name = request.args.get("name", "World")
+    return f'Hello HBNB!, {escape(name)}!'
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
