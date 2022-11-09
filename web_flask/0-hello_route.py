e9a9749421b5a4dbca8e12b5472b620890d3213e
jrichard1909 <jrichard1909@gmail.com>
2022-11-08 19:01:45 -0500

    task 0 fix comment

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
+    """ Function called with / route """
     name = request.args.get("name", "World")
     return f'Hello HBNB!, {escape(name)}!'
 
